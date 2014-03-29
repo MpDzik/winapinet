4687c90ba71f2cc6b4ccc0c5b160e36e7a395eed
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2014-03-29 18:09:27 +0100

    Initial VS solution and project

---
+++ src/WinApiNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="WinAPI.NET">
+//   Copyright (c) Marek Dzikiewicz, All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WinApiNet")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("01efe7d3-2f81-420a-9ba7-290c1d5554e4")]
