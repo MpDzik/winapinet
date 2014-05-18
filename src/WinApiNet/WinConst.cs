00edd643fb0b1ec6b9b8391e02fe33f142ac438e
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2014-05-18 16:47:12 +0200

    WinApiNet: Defined constant for WINAPI S_OK.

---
+++ src/WinApiNet/WinConst.cs
@@ -0,0 +1,13 @@
+﻿namespace WinApiNet
+{
+    /// <summary>
+    /// Well-known Windows constant definitions.
+    /// </summary>
+    public static class WinConst
+    {
+        /// <summary>
+        /// Generic success error code.
+        /// </summary>
+        public const int S_OK = 0x00000000;
+    }
+}
